9dcb95c58afb8806ccfba41efae078a8bbf0bfc6
OlukaDenis <olukadeno@gmail.com>
2020-08-28 20:40:59 +0300

    Add profile UI

--- src/containers/MyProfile.js
+++ src/containers/MyProfile.js
@@ -6,38 +6,65 @@ import RootLayout from '../components/RootLayout';
 import { userShape, houseShape } from '../helpers/propTypeShapes';
 import HouseListItem from '../components/HouseListItem';
 import { formatCurrency } from '../helpers/appUtils';
+import { Container, Row, Col, Media } from 'react-bootstrap';
+import '../styles/UserProfile.scss';
 
 const MyProfile = ({ user, favoriteHouses, expense }) => {
   const UserProfile = () => (
-    <div className="userProfile">
-      <img src={user.image} alt={user.username} />
-      <h3>{user.username}</h3>
-      <p>{user.email}</p>
-      <p>
-        <span>My expense: </span>
-        {formatCurrency(expense)}
-      </p>
-      { user.admin && <Link to="/users"><button type="button">Edit Users</button></Link>}
-    </div>
+    <Col md={10} sm={10} lg={10}>
+      <div className="userProfile">
+        <img 
+          width={200}
+          height={200}
+          style={{borderRadius: '50%'}}
+          src={user.image} 
+          alt={user.username} 
+        />
+        <div className="userInfo">
+          <h4 style={{color: '#333', fontWeight: '700'}}>{user.username}</h4>
+          <p style={{color: '#666'}}>{user.email}</p>
+          <p className="myExpense">
+            <span >Expense: </span>
+            {formatCurrency(expense)}
+          </p>
+          { user.admin && <Link to="/users">
+            <button 
+            type="button"
+            className="btn h-btn h-btn-outlined"
+            >Edit Users</button>
+            </Link>}
+        </div>
+      
+      </div>
+    </Col>
   );
 
   const UserFavorites = () => (
-    <div>
-      <h3> My Favorites</h3>
-      {
-            favoriteHouses.length === 0
-              ? <p>No favorites </p>
-              : favoriteHouses.map(house => (
-                <HouseListItem key={house.id} house={house} />
-              ))
-          }
-    </div>
+    <Col md={10} sm={10} lg={12}>
+       <h5 style={{marginTop: 30, color: '#555', fontWeight: '900', padding: '10px 0'}}>Favorites Houses</h5>
+      <div className="card">
+        <div className="card-body">
+          {
+                favoriteHouses.length === 0
+                  ? <p>No favorites </p>
+                  : favoriteHouses.map(house => (
+                    <HouseListItem key={house.id} house={house} />
+                  ))
+              }
+        </div>
+      </div>
+    </Col>
   );
 
   return (
     <RootLayout>
-      { user && <UserProfile />}
-      <UserFavorites />
+      <Container style={{ paddingTop: 70, marginBottom: 100 }}>
+        <Row>
+          { user && <UserProfile />}
+          <UserFavorites />
+        </Row>
+      </Container>
+      
     </RootLayout>
   );
 };
